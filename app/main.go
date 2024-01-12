721974113256806d57e96602ca33bc1ca11069a3
Manh Tu <xxlaguna93@gmail.com>
2024-01-12 19:54:39 +0800

    filesystem isolation by running chroot

--- app/main.go
+++ app/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"io"
 	"log"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"syscall"
 )
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
@@ -12,15 +15,56 @@ func main() {
 	command := os.Args[3]
 	args := os.Args[4:]
 
+	sandbox, err := os.MkdirTemp("", "sandbox")
+	must(err)
+
+	// copy the command from outside the sandbox, preserving the permissions
+	copyFile(command, sandbox+command)
+
+	syscall.Chroot(sandbox)
+	os.Chdir("/")
+
+	createDevNull()
+
 	cmd := exec.Command(command, args...)
 	cmd.Stderr = os.Stderr
 	cmd.Stdout = os.Stdout
-	err := cmd.Run()
+
+	err = cmd.Run()
 	if exitErr, ok := err.(*exec.ExitError); ok {
 		statusCode := exitErr.ExitCode()
 		os.Exit(statusCode)
 	}
 	must(err)
+}
+
+func copyFile(srcFile, destFile string) {
+	src, err := os.Open(srcFile)
+	must(err)
+	defer src.Close()
+
+	err = os.MkdirAll(filepath.Dir(destFile), 0755)
+	must(err)
+
+	dest, err := os.Create(destFile)
+	must(err)
+	defer dest.Close()
+
+	_, err = io.Copy(dest, src)
+  must(err)
+
+  stat, err := src.Stat()
+	must(err)
+
+  err = dest.Chmod(stat.Mode())
+  must(err)
+}
+
+func createDevNull() {
+	// to avoid problem running in chroot
+	os.Mkdir("/dev", 0755)
+	devNull, _ := os.Create("/dev/null")
+	devNull.Close()
 }
 
 func must(err error) {
